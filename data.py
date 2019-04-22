2761d4b31c9c42bf60b621ad40fe4b7746b12c68
sabrina <lxsabrinahw@gmail.com>
2019-04-22 21:30:06 +0800

    comment

--- data.py
+++ data.py
@@ -10,15 +10,15 @@ with open('label.txt', 'a') as f:
         print('../training/'+imgdir+'/Inco.cfg')
         line = linecache.getline('../training/'+imgdir+'/Inco.cfg', 3)
         print(line)
-        label = line.strip().split()[1]
-        data = nib.load('../training/'+imgdir+'/'+imgdir+'_4d.nii.gz')
-        imgs = data.get_data()
-        x,y,z,t = imgs.shape
-        if not os.path.exists('../train/'+imgdir):
-            os.makedirs('../train/'+imgdir)
-        for i in range(t):
-            for j in range(z):
-                imgname = '../train/'+imgdir+'/'+str(i)+str(j)+'.png'
-                cv2.imwrite(imgname, imgs[:,:,j,i])
-                f.write(imgname+' '+label+'\n')
-                print(imgname+'done!')
+        # label = line.strip().split()[1]
+        # data = nib.load('../training/'+imgdir+'/'+imgdir+'_4d.nii.gz')
+        # imgs = data.get_data()
+        # x,y,z,t = imgs.shape
+        # if not os.path.exists('../train/'+imgdir):
+        #     os.makedirs('../train/'+imgdir)
+        # for i in range(t):
+        #     for j in range(z):
+        #         imgname = '../train/'+imgdir+'/'+str(i)+str(j)+'.png'
+        #         cv2.imwrite(imgname, imgs[:,:,j,i])
+        #         f.write(imgname+' '+label+'\n')
+        #         print(imgname+'done!')
