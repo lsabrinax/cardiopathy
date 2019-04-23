4c1ff58289e10869532c0784c7f02d9652494f75
sabrina <lxsabrinahw@gmail.com>
2019-04-23 19:23:48 +0800

    product test img

--- data.py
+++ data.py
@@ -3,24 +3,30 @@ import nibabel as nib
 import cv2
 import linecache
 
-imgdirs = os.listdir('../training/')
-print('imgdirs length is %d' % len(imgdirs))
 
-with open('label.txt', 'a') as f:
-    for imgdir in imgdirs:
-        print(imgdir)
-        # print('../training/'+imgdir+'/Info.cfg')
-        line = linecache.getline('../training/'+imgdir+'/Info.cfg', 3)
-        # print(line)
-        label = line.strip().split()[1]
-        data = nib.load('../training/'+imgdir+'/'+imgdir+'_4d.nii.gz')
-        imgs = data.get_data()
-        x,y,z,t = imgs.shape
-        if not os.path.exists('../train/'+imgdir):
-            os.makedirs('../train/'+imgdir)
-        for i in range(t):
-            for j in range(z):
-                imgname = '../train/'+imgdir+'/'+str(i*z+j+1)+'.png'
-                cv2.imwrite(imgname, imgs[:,:,j,i])
-                f.write(imgname+' '+label+'\n')
-                print(imgname+'done!')
+
+    imgdirs = os.listdir('../testing/')
+    print('imgdirs length is %d' % len(imgdirs))
+
+    with open('../testing/label.txt', 'a') as f:
+        for imgdir in imgdirs:
+            print(imgdir)
+            # print('../testing/'+imgdir+'/Info.cfg')
+            # line = linecache.getline('../testing/'+imgdir+'/Info.cfg', 3)
+            # print(line)
+            # label = line.strip().split()[1]
+            data = nib.load('../testing/'+imgdir+'/'+imgdir+'_frame01.nii.gz')
+            imgs = data.get_data()
+            x,y,z= imgs.shape
+            if not os.path.exists('../test/'+imgdir):
+                os.makedirs('../test/'+imgdir)
+            # for i in range(t):
+                for j in range(z):
+                    imgname = '../test/'+imgdir+'/'+str(j+1)+'.png'
+                    cv2.imwrite(imgname, imgs[:,:,j,i])
+                    f.write(imgname+' '+'\n')
+                    print(imgname+'done!')
+
+
+
+
