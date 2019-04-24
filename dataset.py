356a7f79c2a017743aa00b5d957d30ded1c33b84
sabrina <lxsabrinahw@gmail.com>
2019-04-24 09:39:38 +0800

    add test funciton

--- dataset.py
+++ dataset.py
@@ -4,17 +4,22 @@ from PIL import Image
 
 class CMRDataset(Dataset):
 	"""docstring for CMRDataset"""
-	def __init__(self, labelfile='label.txt', transforms=None):
+	def __init__(self, labelfile='label.txt', transforms=None, dtype='train'):
 		# super(CMRDataset, self).__init__()
 		with open(labelfile, 'r') as f:
 			gt = f.readlines()
 		self.gt = gt
 		self.label2idx = {'NOR': 0, 'MINF': 1, 'DCM': 2, 'HCM': 3, 'RV': 4}
 		self.transforms = transforms
+		self.dtype = dtype
 	
 	def __getitem__(self, index):
-		line = self.gt[index]
-		imgpath, label = tuple(line.strip().split())
+		line = self.gt[index].strip()
+		if self.dtype == 'train':
+			imgpath, label = tuple(line.split())
+			label = self.label2idx[label]
+		imgpath = line
+		
 		try:
 			img = Image.open(imgpath)
 		except:
@@ -22,7 +27,8 @@ class CMRDataset(Dataset):
 			return self[index+1]
 		if self.transforms:
 			img = self.transforms(img)
-		label = self.label2idx[label]
+		if self.dtype == 'test':
+			label = imgpath
 		return img, label
 
 	def __len__(self):
--- train.py
+++ train.py
@@ -9,6 +9,14 @@ import visdom
 import utils
 import torch
 import torch.backends.cudnn as cudnn
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--labelfile', required=True)
+parser.add_argument('--type', required=True, help='train or test')
+parser.add_argument('--pretrained', default='')
+
+opt = parser.parse_args()
 
 model = resnet34(pretrained=True)
 model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
@@ -16,6 +24,9 @@ nn.init.kaiming_normal_(model.conv1.weight, mode='fan_out', nonlinearity='relu')
 # nn.init.constant_(model.features[0].bias, 0)
 model.fc = nn.Linear(in_features=512, out_features=5, bias=True)
 
+if opt.type == 'test':
+	print('load pretrained model from %s' % opt.pretrained)
+	model.load_state_dict(torch.load(opt.pretrained))
 transform = T.Compose([
 	T.Resize(224),
 	T.CenterCrop(224),
@@ -23,8 +34,10 @@ transform = T.Compose([
 	T.Normalize(mean=[0.5], std=[0.5])
 ])
 
-train_dataset = dataset.CMRDataset(transforms=transform)
-dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=2)
+
+idx2label = {0: 'NOR', 1: 'MINF', 2: 'DCM', 3: 'HCM', 4: 'RV'}
+data_set = dataset.CMRDataset(labelfile=opt.lablefile, transforms=transform, dtype=opt.type)
+dataloader = DataLoader(data_set, batch_size=32, shuffle=True, num_workers=2)
 optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=5e-4)
 criterion = nn.CrossEntropyLoss()
 loss_meter = utils.averager()
@@ -37,8 +50,10 @@ if use_gpu:
 
 
 def train():
+	model.train()
 	vis = visdom.Visdom(env='CMR', port=8893)
 	mes = ''
+	
 	for epoch in range(20):
 		loss_meter.reset()
 
@@ -62,7 +77,24 @@ def train():
 				vis.text(mes, win='text', opts={'title': 'display_message'})
 		torch.save(model.state_dict(), 'expr/restnet34_%d.pth' % epoch)
 
+def test():
+	model.eval()
+	# nsample = 0
+	for img, imgpath in dataloader:
+		# nsample += img.size(0)
+		if use_gpu:
+			img = img.cuda()
+			pred = model(img)
+			prob = nn.functional.sofmax(pred, dim=1)
+			res, index = torch.topk(prob, 1)
+			with open('pred.txt', 'a') as f:
+				for path, idx in zip(imgpath, index):
+					label = idx2label[idx.item()]
+					f.write(path+' '+label+'\n')
 
 if __name__ == '__main__':
-	train()
+	if opt.type == 'train':
+		train()
+	elif opt.type == 'test':
+		test()
 
