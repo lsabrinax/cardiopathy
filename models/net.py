5d6cfd90306214c3aa7b53709f7a504d65eb617d
sabrina <lxsabrinahw@gmail.com>
2019-05-23 19:09:16 +0800

    change model

--- models/net.py
+++ models/net.py
@@ -5,7 +5,7 @@ class FeatureNet(nn.Module):
 	def __init__(self, inchannel, outchannel):
 		super(FeatureNet, self).__init__()
 		self.features = nn.Sequential(
-			nn.Linear(inchannel, 10),
+			nn.Linear(inchannel, 5),
 			nn.ReLU(inplace=True),
 			nn.Linear(10, outchannel),
 		)
