97ffff12f0c92d9cd32de9e5446010c9d6a35317
sabrina <lxsabrinahw@gmail.com>
2019-05-23 15:46:15 +0800

    net module

---
+++ models/net.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+class FeatureNet(nn.Module):
+
+	def __init__(self, inchannel, outchannel):
+		self.features = nn.Sequential(
+			nn.Linear(inchannel, 10),
+			nn.ReLU(inplace=True),
+			nn.Linear(10, outchannel),
+			nn.Softmax(dim=1)
+		)
+
+	def forward(self, x):
+		x = self.features(x)
+		return x
