736ba77d4d4f9020bfe6dc44c225274b0f122e51
sabrina <lxsabrinahw@gmail.com>
2019-05-24 11:04:50 +0800

    add batchnormalize

--- models/net.py
+++ models/net.py
@@ -5,6 +5,7 @@ class FeatureNet(nn.Module):
 	def __init__(self, inchannel, outchannel):
 		super(FeatureNet, self).__init__()
 		self.features = nn.Sequential(
+			nn.BatchNorm1d(inchannel),
 			nn.Linear(inchannel, 5),
 			nn.ReLU(inplace=True),
 			# nn.Linear(10, 5),
